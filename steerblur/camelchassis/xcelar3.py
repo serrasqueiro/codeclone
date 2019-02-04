e732fb0d27f11a8cac296864c1a98c5c64bad02b
serrasqueiro <h@serrasqueiro.com>
2019-02-04 16:05:34 +0000

    AB dump compatible for Python 2 and 3; excel dates simple conversions.

--- steerblur/camelchassis/xcelar3.py
+++ steerblur/camelchassis/xcelar3.py
@@ -1,11 +1,12 @@
 # xcelar3.py  (c)2019  Henrique Moreira (part of 'camelchassis')
 
 """
-  Simple Excel readout.
+  Simple Excel readout. Dumps 'AB' also.
 
   Compatibility: python 2 and 3.
 """
 
+import datetime
 import codecs
 import zipfile
 from xml.etree.ElementTree import iterparse
@@ -17,10 +18,12 @@ from xml.etree.ElementTree import iterparse
 def test_xcelar3 (outFile, args):
   validCmd = False
   code = 0
+  verbose = 0
   cmdStr = args[ 0 ]
   param = args[ 1: ]
   out = outFile
-  showWarn = True
+  showWarn = False
+  showBin = False
   if cmdStr=="xump":
     validCmd = True
     if out==sys.stdout:
@@ -33,13 +36,25 @@ def test_xcelar3 (outFile, args):
         out.write( s + '\n' )
   if cmdStr=="abmp":
     validCmd = True
-    if out==sys.stdout:
+    any = True
+    while len( param[ 0 ] )>0 and any:
+      any = False
+      if param[ 0 ].find( "-v" )==0:
+        verbose += param[ 0 ].count( "v" )
+        any = True
+        del param[ 0 ]
+        continue
+      if param[ 0 ]=="-b":
+        showBin = True
+    showWarn = verbose>0
+    if out==sys.stdout and showBin:
       sys.stdout = rewrite( "" )
       out = sys.stdout
     for filename in param:
       rows = xcel_xlx_read( filename )
-      aSep = " "
+      aSep = " " if verbose<=0 else " ; " if verbose>=2 else ";"
       idx = 0
+      hasDate = False
       for r in rows:
         idx += 1
         linear = None
@@ -53,18 +68,32 @@ def test_xcelar3 (outFile, args):
         else:
           s = "#"+str(len(r)) + ";"
         if linear:
-          excDate = '{:>9}'.format( r['A'] )
+          exDateStr = r['A']
+          isHeader = exDateStr.find( "Data" )==0
+          tupDate = from_xcel_date( exDateStr )
+          #print("isHeader:", isHeader, "; hasDate:", hasDate, "tupDate:", tupDate)
+          if hasDate and tupDate[ 0 ]!=0:
+            aDate = tupDate[ 1 ]
+          else:
+            aDate = "#"+exDateStr
+          fmtDate = '{:>10}'.format( aDate )
           v1 = deval( m1 )
           v2 = deval( m2 )
           val1Str = '{:9.2f}'.format( v1 )
           val2Str = '{:9.2f}'.format( v2 )
-          isHeader = r['A'].find( "Data" )==0
           if isHeader:
+            hasDate = True
             val1Str = '{:9}'.format("Valor")
             val2Str = '{:9}'.format("Saldo")
-            continue
-          s = excDate + aSep + val1Str + aSep + val2Str + aSep + r['C']
-          out.write( s + "\n" )
+            if verbose<3:
+              continue
+          if isHeader:
+            desc = "Descricao"
+          else:
+            desc = simpler_desc( r['C'] )
+          s = fmtDate + aSep + val1Str + aSep + val2Str + aSep + desc
+          out.write( s )
+          out.write( "\n" )
         else:
           lineStr = "Line " + str( idx )+": "
           if showWarn:
@@ -127,6 +156,44 @@ def xcel_xlx_read (filename, sheets=[]):
       rows.append( row )
       row = {}
   return rows
+
+
+#
+# from_xcel_date()
+#
+def from_xcel_date (sXcelDate):
+  # xcel date 42139 is ordinal 735733
+  fail = False
+  minDate = 42139
+  try:
+    i = int( sXcelDate )
+  except:
+    fail = True
+  if fail or i<minDate or i>99999:
+    return (0, sXcelDate)
+  oDate = i - minDate + 735733
+  return (oDate, xcel_str_date( datetime.datetime.fromordinal( oDate )))
+
+
+#
+# xcel_str_date()
+#
+def xcel_str_date (dttm, sep='-'):
+  s = '{:04}'.format( dttm.year ) + sep + '{:02}'.format( dttm.month ) + sep + '{:02}'.format( dttm.day )
+  return s
+
+
+#
+# simpler_desc()
+#
+def simpler_desc (s):
+  res = s
+  any = True
+  while any:
+    keep = res
+    res = res.replace( "  ", " " ).replace( ";", ":," )
+    any = keep!=res
+  return res
 
 
 #
